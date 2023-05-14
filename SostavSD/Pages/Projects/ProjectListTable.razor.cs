0ff3c9abcfeedaab62a6102b883e5b00e52a83cd
Alena-Samus <elensawelu@gmail.com>
2023-05-14 22:24:35 +0300

    Add component ProjectListTable

---
+++ SostavSD/Pages/Projects/ProjectListTable.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Localization;
+using SostavSD.Interfaces;
+using SostavSD.Models;
+using SostavSD.Pages.Contracts;
+
+namespace SostavSD.Pages.Projects
+{
+    partial class ProjectListTable
+    {
+        private IProjectService _projectService;
+        private IStringLocalizer<ProjectModel> _localizer;
+
+        private List<ProjectModel> _projects;
+        string searchString;
+
+        public ProjectListTable(IProjectService projectService, IStringLocalizer<ProjectModel> localizer)
+        {
+            _projectService = projectService;
+            _localizer = localizer;
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            _projects = await _projectService.GetProjectsAsync();
+        }
+
+        private bool FilterFuncCurrent(ProjectModel project) => FilterFunc(project, searchString);
+        private bool FilterFunc(ProjectModel project, string searchString)
+        {
+            bool result = false;
+            return result;
+        }
+    }
+}
