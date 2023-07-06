d4df689d4eb8d18ef37ef0f45f3885518f968639
Alena Samus <elensawelu@gmail.com>
2023-07-06 16:42:56 +0300

    Add IndividualProject.razor.css file

---
+++ SostavSD/Pages/ProjectSelection/IndividualProject.razor.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using SostavSD.Interfaces;
+using SostavSD.Models;
+using SostavSD.Services;
+
+namespace SostavSD.Pages.ProjectSelection
+{
+	partial class IndividualProject
+	{
+		[Parameter] public ProjectModel	Project { get; set; }
+		[Parameter] public List<ManagerUserModel> Calculator { get; set; }
+	
+		[Inject] public IAuthorizedUserService AuthorizedUserService { get; set; }
+
+		string calculatorName;
+
+
+		protected override async Task OnInitializedAsync()
+		{
+
+			calculatorName = Calculator.FirstOrDefault(x => x.UserId == Project.Contract.CalculatorId).UserSurname.ToString();
+
+		}
+	}
+}
--- SostavSD/Pages/ProjectSelection/ProjectSelection.razor.cs
+++ SostavSD/Pages/ProjectSelection/ProjectSelection.razor.cs
@@ -1,6 +1,30 @@
-﻿namespace SostavSD.Pages.ProjectSelection
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using SostavSD.Interfaces;
+using SostavSD.Models;
+
+namespace SostavSD.Pages.ProjectSelection
 {
     partial class ProjectSelection
     {
+        [Inject] public IEntityManagementService EntityManagementService { get; set; }
+		[Inject] public IAuthorizedUserService AuthorizedUserService { get; set; }
+		[Inject] public IStringLocalizer<ProjectSelection> Localizer { get; set; }
+
+        List<ManagerUserModel> Calculators = new();
+
+        private List<ProjectModel> _projects = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetProjectsAsync();
+			Calculators = await AuthorizedUserService.GetAllUsersAsync();
+		}
+
+        private async Task <List<ProjectModel>> GetProjectsAsync()
+        {
+            _projects = await EntityManagementService.GetProjectsAsync();
+            return _projects;
+        }
     }
 }
