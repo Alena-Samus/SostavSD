6d95595c1ccfd8cd2cecdfef8bc1d02e6253be43
Alena Samus <elensawelu@gmail.com>
2025-10-13 16:15:24 +0300

    Created Cancel adn Saved buttons

--- SostavSD/Pages/ProjectSostav/NewDrawing.razor.cs
+++ SostavSD/Pages/ProjectSostav/NewDrawing.razor.cs
@@ -1,6 +1,44 @@
-﻿namespace SostavSD.Pages.ProjectSostav
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Microsoft.VisualBasic;
+using MudBlazor;
+using SostavSD.Interfaces;
+using SostavSD.Models;
+
+namespace SostavSD.Pages.ProjectSostav
 {
     partial class NewDrawing
     {
+        [Inject] IEntityManagementService EntityManagementService { get; set; }
+        [Inject] IStringLocalizer<DrawingsWithoutEstimates> Localizer { get; set; }
+        [Inject] ISnackbar Snackbar { get; set; }
+
+        private NavigationManager _navigationManager;
+
+        private DrawingModel _newDrawing = new();
+
+
+        List<DrawingModel> _newDrawingsList = new List<DrawingModel>();
+
+        protected override async Task OnInitializedAsync()
+        {
+            _newDrawing = new DrawingModel();
+        }
+        public NewDrawing(NavigationManager navigationManager)
+        {
+            _navigationManager = navigationManager;
+        }
+
+        private void Save()
+        {
+            Snackbar.Add("Saved", Severity.Success);
+            GoBack();
+        }
+
+        private void GoBack()
+        {
+            Snackbar.Add("Canceled", Severity.Warning);
+            _navigationManager.NavigateTo("javascript:history.back()", forceLoad: true);
+        }
     }
 }
