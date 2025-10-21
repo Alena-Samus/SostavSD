155fe43573fe7c089a64fbf7ce0f7a225da2f826
Alena Samus <elensawelu@gmail.com>
2025-10-21 11:27:33 +0300

    Add ProjectCard component. Navigate to the ProjectCard page with parameter ProjectId

--- SostavSD/Pages/ProjectSostav/Index.razor.cs
+++ SostavSD/Pages/ProjectSostav/Index.razor.cs
@@ -3,7 +3,23 @@
 namespace SostavSD.Pages.ProjectSostav
 {
 	partial class Index
+
 	{
-		[Parameter] public int ProjectId {get;set;}
+        [Inject] public NavigationManager NavigationManager { get; set; }
+        [Parameter] public int ProjectId {get;set;}
+
+        private void Navigate()
+        {
+            try
+            {
+                NavigationManager.NavigateTo($"/projectcard/{ProjectId}");
+            }
+            catch (Exception ex)
+            {
+                //logger.Error(ex);
+                throw;
+            }
+
+        }
     }
 }
---
+++ SostavSD/Pages/ProjectSostav/ProjectCard/ProjectCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SostavSD.Pages.ProjectSostav.ProjectCard
+{
+    public partial class ProjectCard
+    {
+        [Parameter] public int ProjectId { get; set; }
+
+    }
+}
