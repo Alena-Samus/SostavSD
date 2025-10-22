be1363353e81deb4802c6976d46bea5782735f31
Alena Samus <elensawelu@gmail.com>
2025-10-22 16:29:57 +0300

    Select Chapter from List

--- SostavSD/Pages/ProjectSostav/ProjectCard/AddNewSubsections.razor.cs
+++ SostavSD/Pages/ProjectSostav/ProjectCard/AddNewSubsections.razor.cs
@@ -1,10 +1,47 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using SostavSD.Interfaces;
+using SostavSD.Models;
 using System.Reflection.Metadata;
 
 namespace SostavSD.Pages.ProjectSostav.ProjectCard
 {
     public partial class AddNewSubsections
     {
+        [Inject] IChapterService ChapterService { get; set; }
+        [Inject] IStringLocalizer<AddNewSubsections> Localizer { get; set; }
         [Parameter] public int ProjectId { get; set; }
+
+        private List<ChapterModel> chapters = new List<ChapterModel>();
+        
+        
+        private string Country { get; set; } = "РБ";
+        private int SelectedChapter { get; set; }
+       
+        
+        protected override async Task OnInitializedAsync()
+        {
+            chapters = await ChapterService.GetChaptersByCountryAsync(Country);
+            //Chapter = new ChapterModel();
+        }
+
+        private async Task OnSelectedOptionChanged(string selectedOption)
+        {
+            Country = selectedOption;
+            await GetChapters(Country);
+            // call your stuff
+        }
+
+        private async Task GetChapters (string country)
+        {
+            chapters = await ChapterService.GetChaptersByCountryAsync(country);
+        }
+
+        private async Task OnGroupSelected(int selectedChapter)
+        {
+            //Chapter = chapters.FirstOrDefault(e => e.ChapterId == selectedChapter);
+            //SelectedChapter = Chapter.ChapterName;
+        }
+
     }
 }
