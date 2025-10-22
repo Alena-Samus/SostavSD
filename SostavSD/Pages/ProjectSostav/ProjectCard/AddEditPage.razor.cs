e40885282717dcc74d727448ea65ce9b02dc2a90
Alena-Samus <elensawelu@gmail.com>
2025-10-23 00:30:45 +0300

    Change ProjectCardChapters

--- SostavSD/Pages/ProjectSostav/ProjectCard/AddEditPage.razor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace SostavSD.Pages.ProjectSostav.ProjectCard
-{
-    public partial class AddEditPage
-    {
-        [Parameter] public int ProjectId { get; set; }
-    }
-}
